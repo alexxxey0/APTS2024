ab40df5fd44aa9ec3bbedfb8f13116fbb67f3bb8
alexxxey <aleksejs.gorlovics@gmail.com>
2024-02-29 15:37:10 +0200

    making progress, just need to sort the output

--- muita/muita.cpp
+++ muita/muita.cpp
@@ -41,18 +41,39 @@ int first_free_guard(vector<Guard> guards, int guards_count) {
     return 0;
 }
 
-int get_guard_time(int *guards, int guards_count, int id) {
-    for (int i = 0; i <= guards_count; i++) {
-        if (guards[i] == id) return guards[i];
+int get_guard_time(vector<Guard> guards, int guards_count, int id) {
+    for (int i = 0; i < guards_count; i++) {
+        if (guards[i].id == id) return guards[i].time;
     }
     return 0;
+}
+
+int get_guard_available_at(vector<Guard> guards, int guards_count, int id) {
+    for (int i = 0; i < guards_count; i++) {
+        if (guards[i].id == id) return guards[i].available_at;
+    }
+    return 0;
+}
+
+int get_soonest_guard (vector<Guard> guards, int guards_count) {
+    int min_time = guards[0].available_at;
+    int guard_id = guards[0].id;
+    for (int i = 0; i < guards_count; i++) {
+        //cout << guards[i].available_at << " " << min_time << endl;
+        if (guards[i].available_at < min_time) {
+            guard_id = guards[i].id;
+            min_time = guards[i].available_at;
+        }
+    }
+    cout << endl;
+    return guard_id;
 }
 
 int main() {
     ifstream cin("customs.txt");
     ofstream cout("customs_out.txt");
 
-    int p_guards_count, n_guards_count, p_time, n_time, id, time, available_guard;
+    int p_guards_count, n_guards_count, p_time, n_time, id, time, available_guard, soonest_guard, waiting_time;
     char letter1, letter2;
     vector<Person_out> output_p;
     vector<Person_out> output_n;
@@ -89,6 +110,7 @@ int main() {
 
     while (letter1 == 'P' || letter1 == 'N') {
         cin >> id;
+        waiting_time = 0;
 
         if (letter1 == 'P') {
             for (Guard &g: p_guards) {
@@ -101,6 +123,11 @@ int main() {
                 p_guards[available_guard - 1].available_at = id + p_guards[available_guard - 1].time;
                 Person_out person_out(id, id + p_guards[available_guard - 1].time);
                 output_p.push_back(person_out);
+            } else {
+                soonest_guard = get_soonest_guard(p_guards, p_guards_count);
+                p_guards[soonest_guard - 1].available_at = p_guards[soonest_guard - 1].available_at + p_guards[soonest_guard - 1].time;
+                Person_out person_out(id, get_guard_available_at(p_guards, p_guards_count, soonest_guard));
+                output_p.push_back(person_out);
             }
 
         } else if (letter1 == 'N') {
@@ -108,19 +135,21 @@ int main() {
                 if (g.available_at <= id) g.busy = false;
             }
             available_guard = first_free_guard(n_guards, n_guards_count);
-
             if (available_guard != 0) {
                 n_guards[available_guard - 1].busy = true;
                 n_guards[available_guard - 1].available_at = id + n_guards[available_guard - 1].time;
                 Person_out person_out(id, id + n_guards[available_guard - 1].time);
+                output_n.push_back(person_out);
+            } else {
+                soonest_guard = get_soonest_guard(n_guards, n_guards_count);
+                n_guards[soonest_guard - 1].available_at = n_guards[soonest_guard - 1].available_at + n_guards[soonest_guard - 1].time;
+                Person_out person_out(id, get_guard_available_at(n_guards, n_guards_count, soonest_guard));
                 output_n.push_back(person_out);
             }
         }
         cin >> letter1;
     }
 
-    //for (int i = 0; i < p_guards_count; i++) cout << "P " << p_guards[i].id << " " << p_guards[i].time << endl;
-    //for (int i = 0; i < n_guards_count; i++) cout << "N " << n_guards[i].id << " " << n_guards[i].time << endl;
     for (Person_out po: output_p) {
         cout << po.id << " " << po.time << endl;
     }
